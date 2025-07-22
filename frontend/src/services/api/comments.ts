84814eb2e2cd3e4026fdfd933e1edf5a3e7a6632
RebeccaBrasileiro <rebeccabrasileiro28@gmail.com>
2025-07-22 19:36:11 -0300

    teste6

--- frontend/src/services/api/comments.ts
+++ frontend/src/services/api/comments.ts
@@ -6,9 +6,6 @@ export interface IComment {
     date?: string
 }
 class CommentData {
-     getAll() {
-        return api.get('/comments/')
-    }
     getByUser() {
         return api.get(`/comments/user/`)
     }
